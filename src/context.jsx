10497ec1af63b436929d62a081fb684ac97cdb2a
fsuh <92304761+fsuh@users.noreply.github.com>
2023-07-05 13:23:32 +0300

    add searchform and gallery logic

--- src/context.jsx
+++ src/context.jsx
@@ -1,19 +1,32 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
+const getInitialDarkMode = () => {
+	const preferDarkMode = window.matchMedia(
+		"(prefers-color-scheme:dark)"
+	).matches;
+	const storedDarkMode = localStorage.getItem("darkTheme") === "true";
+	return storedDarkMode || preferDarkMode;
+};
 
 export const AppProvider = ({ children }) => {
-	const [isDarkTheme, setIsDarkTheme] = useState(false);
+	const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+	const [searchTerm, setSearchTerm] = useState("tortoise");
 
 	const toggleDarkTheme = () => {
 		const newDarkTheme = !isDarkTheme;
 		setIsDarkTheme(newDarkTheme);
-		const body = document.querySelector("body");
-		body.classList.toggle("dark-theme", newDarkTheme);
-		console.log(body);
+		localStorage.setItem("darkTheme");
+		// const body = document.querySelector("body");
+		// body.classList.toggle("dark-theme", newDarkTheme);
 	};
+	useEffect(() => {
+		document.body.classList.toggle("dark-theme", isDarkTheme);
+	}, [isDarkTheme]);
 	return (
-		<AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+		<AppContext.Provider
+			value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+		>
 			{children}
 		</AppContext.Provider>
 	);
